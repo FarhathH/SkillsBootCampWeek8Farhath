8938381fb465674376fda1d63ae3db8f826f62bc
FarhathH <farhathh0@gmail.com>
2023-12-05 16:38:18 +0000

    some testing

--- current/src/App.test.js
+++ current/src/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import {Currency, Pound, Dollar, Converter, Money} from './money';
+import {CurrencyTable, Converter, Money, MoneyFactory} from './money';
 
 
 /*
@@ -8,6 +8,8 @@ import {Currency, Pound, Dollar, Converter, Money} from './money';
   Test samples to check if components work.
 
 */
+
+
 test("Money has value", ()=>
   {
     let coin = new Money(50);
@@ -17,46 +19,96 @@ test("Money has value", ()=>
 
 test("Pound has value", ()=>
   {
-    let poundCurrency = new Pound(0.79);
+    let poundCurrency = MoneyFactory.pound(0.79);
     expect(poundCurrency.value).toBe(0.79)
   }
 )
 
 test("Dollar has value", ()=>
   {
-    let dollarCurrency = new Dollar(1);
-    expect(dollarCurrency.value).toBe(1)
+    let dollarCurrency = MoneyFactory.UsDollar(1.26);
+    expect(dollarCurrency.value).toBe(1.26)
   }
 )
 
 test("Dollar has value of 15", ()=>
   {
-    let dollarCurrency = new Dollar(15);
+    let dollarCurrency = MoneyFactory.UsDollar(15);
     expect(dollarCurrency.value).toBe(15)
+  }
+)
+
+test("Money factory can create a currency",()=>
+  {
+    
+    expect(MoneyFactory.pound(15)).toBeInstanceOf(Money)
+    expect(MoneyFactory.UsDollar(10)).toBeInstanceOf(Money)
   }
 )
 
 test("Pound can be converted into dollar", ()=>
   {
-    let pound = new Pound(10);
+    let pound = MoneyFactory.pound(10);
     let converter = new Converter();
-    expect(converter.convertToDollar(pound)).toBeInstanceOf(Dollar);
+    expect(converter.convertToDollar(pound)).toBeInstanceOf(Money);
   }
 )
 
-test( "converter returns value", ()=>
+test("Dollar can be converted into pound", ()=>
   {
+    let dollar = MoneyFactory.UsDollar(10);
     let converter = new Converter();
-    let pound = new Pound(10)
-
-    expect(converter.returnValue(pound)).toBe(10);
+    expect(converter.convertToPound(dollar)).toBeInstanceOf(Money);
   }
 )
 
-test("Money factiry can create a dollar",()=>
+
+test("Money has currency",()=>
   {
-    moneyFactory = new MoneyFactory();
-    expect(moneyFactory.dollar(10)).toBeInstanceOf(Dollar)
+    let dollar = MoneyFactory.UsDollar(10)
+    let pound = MoneyFactory.pound(32)
+    expect(dollar.currency).toBe('usd')
+    expect(pound.currency).toBe('gbp')
+
+  }
+  
+)
+
+let values = {
+  'usd':1.26,
+  'gbp':0.79
+}
+
+let currentTable = new CurrencyTable('usd',values);
+
+test("Currency table has values", ()=>
+  { 
+    expect(currentTable.currency).toBe('usd')
+    expect(currentTable.table.gbp).toBe(0.79)
+  }
+)
+
+test("Converter can store tables",()=>
+  {
+    let converter = new Converter();
+    converter.addTable(currentTable);
+    expect(converter.tables.includes(currentTable)).toBe(true)
+  }
+)
+
+test("gbp can be found by string", ()=>
+  {
+    expect(currentTable.table['gbp']).toBe(0.79)
+  }
+)
+
+test("Currency can be converted", () =>
+  {
+    let converter = new Converter();
+    converter.addTable(currentTable);
+    let dollar = converter.exchange(MoneyFactory.pound(1), 'usd');
+    expect(dollar.value).toBe(1.26)
+    expect(dollar.currency).toBe('usd')
   }
 )
 
--- current/src/money.js
+++ current/src/money.js
@@ -1,32 +1,72 @@
-export class Converter{
+export class Converter{ //class for converting currencies
     convertToDollar(pound){
-        return new Dollar(pound.value)
+        return new Money(pound.value)
     }
 
-    returnValue(pound){
+    convertToPound(UsDollar){
+        return new Money(UsDollar.value)
+    }
+
+    returnValueDollar(UsDollar){
+        return UsDollar.value;
+    }
+
+    returnValuePound(pound){
         return pound.value;
     }
+
+    constructor(tables){
+        this.tables = [];
+    }
+
+    addTable(table){
+      this.tables.push(table);
+    }
+
+    exchange(money, currency){
+        let exchangeTable = this.tables.find((element) => element.currency === currency)
+        let exchangeValue = exchangeTable.table[money.currency]
+        return new Money(currency, money.value * exchangeValue)
+    }
 }
 
-export class Money{
-    constructor(value){
+export class CurrencyTable{
+    constructor(currency, table){
+        this.currency = currency;
+        this.table = table;
+    }
+}
+
+export class Money{ //class for generating money.
+    constructor(value, currency){
         this.value = value;
+        this.currency = currency;
     }
 }
 
-export class MoneyFactory{
-    dollar(value){
-        return new Dollar(value);
+export class MoneyFactory{ //class for creating money
+    static UsDollar(value){
+        return new Money(value,'usd');
+    }
+
+    static pound(value){
+        return new Money(value, 'gbp');
+    }
+
+    static euro(value){
+        return new Money(value, 'eur');
+    }
+
+    static yen(value){
+        return new Money(value, 'yen');
+    }
+
+    static australianDollar(value){
+        return new Money(value, 'yen');
     }
 }
 
 
-export class Pound extends Money{
 
-}
-
-export class Dollar extends Money{
-    
-}
 
 
