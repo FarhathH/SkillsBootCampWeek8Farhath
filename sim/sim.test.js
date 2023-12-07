4378b5bf403cdf424a3ce36250d717cacae7915a
Farhath Hussain <farhath.hussain17@bathspa.ac.uk>
2023-12-07 13:30:33 +0000

    some code for the test file

---
+++ sim/sim.test.js
@@ -0,0 +1,17 @@
+//function to add digits
+function addThree(num1, num2, num3){
+    return num1 + num2 * num3
+}
+
+//values being tested
+let num1 = 1
+let num2 = 2
+let num3 = 3
+
+//variables to use in test
+let failedMessage = "You suck"
+let expectedOutput = 6
+let actualOutput = addThree(num1, num2, num3)
+
+//the test
+console.assert(actualOutput == expectedOutput,{failedMessage})
