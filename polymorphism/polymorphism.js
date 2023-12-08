5c8d6194aae3f4e1a415ed21c93e89b9311a7dcf
Farhath Hussain <farhathh0@gmail.com>
2023-12-08 11:17:29 +0000

    added another class

--- polymorphism/polymorphism.js
+++ polymorphism/polymorphism.js
@@ -10,19 +10,29 @@ class Vehicle{//for vehicle attributes
     }
 }
 
+class Car extends Vehicle{ //for car attributes
+    constructor(brand, model, passengerNo){
+        super(brand, model, passengerNo)
+        this.speed = 101
+    }
+}
 
 class Truck extends Vehicle{ //for truck
     constructor(brand, model, passengerNo){
         super(brand, model, passengerNo); //should enable mother class attributes.
         this.cargo = []
+        this.speed = 95
     }
 }
 
 //instantiated object
 let t1 = new Truck("Scania", "Curtain Side", 2);
+let c1 = new Car("Seat", "Hatchback", 5);
+
 
 //called it
 console.log(t1.model)
+console.log(c1.brand)
 
 //allowing truck to make a sound
-v1.MakeSound()
+t1.MakeSound()
